2613b577d86910aaf9410c89b7dcddeff2163f30
Paul1336 <80555192+Paul1336@users.noreply.github.com>
2025-03-27 23:48:02 +0800

    Update agent.py

--- agent.py
+++ agent.py
@@ -4,29 +4,46 @@ import copy
 from env import Env, State, Experiance
 import logger
 
-TEMPERATURE = 1
-
 class DuelDDQNAgent():
     Q_main = None
     Q_target = None
     optimizer = None
     discount_factor = None
 
-    def __init__(self, Q_model, optimizer, discount_factor:float=0.99):
-        self.Q_main = Q_model
-        self.Q_target = Q_model
+    def __init__(self, Q_model, optimizer, discount_factor:float=0.99, temperature: float = 1):
+        self.Q_main = copy.deepcopy(Q_model)
+        self.Q_origin = copy.deepcopy(Q_model)
+        self.Q_target = copy.deepcopy(Q_model)
         self.optimizer = optimizer
         self.discount_factor = discount_factor
+        self.temperature = temperature
         self.log = logger.get_logger(__name__)
 
+
+    def choose_action_origin(self, state: State, epsilon: float) ->int:
+        self.log.debug("pretrained action\n")
+        with torch.no_grad():
+            legal_actions = Env.action_space(state)
+            filtered_values = self.pred_model(state, "origin")[legal_actions]
+            return legal_actions[torch.argmax(filtered_values).item()]
+
+    
     def choose_action(self, state: State, epsilon: float) ->int:
         #print("choosing action:\n")
+        #print("epsilon = ", epsilon)
+        self.log.debug(f"epsilon = {epsilon}\n")
         if np.random.uniform(0, 1) < epsilon:
             legal_actions = Env.action_space(state)
-            filtered_values = self.pred_model(state, "main")[legal_actions]
-            probabilities = torch.softmax(filtered_values / TEMPERATURE, dim=0)
-            chosen_index = torch.multinomial(probabilities, num_samples=1)
-            return legal_actions[chosen_index.item()]
+            self.log.debug(f"legal_actions = {legal_actions}\n")
+            chosen_index = np.random.choice(len(legal_actions))
+            return legal_actions[chosen_index]
+            #filtered_values = self.pred_model(state, "main")[legal_actions]
+            #filtered_values = filtered_values - torch.max(filtered_values)
+            #self.log.debug(f"filtered_values = {filtered_values}\n")
+            #probabilities = torch.softmax(filtered_values / self.temperature, dim=0)
+            #self.log.debug(f"probabilities = {probabilities}\n")
+            #chosen_index = torch.multinomial(probabilities, num_samples=1)
+            #return legal_actions[chosen_index.item()]
             #return Env.random_action(state)
         else:
             self.log.debug("predicted action\n")
@@ -42,8 +59,10 @@ class DuelDDQNAgent():
     def pred_model(self, state, target) -> State:
         if target == "target":
             return self.Q_target(state)
-        else:
+        elif target == "main":
             return self.Q_main(state)
+        else:
+            return self.Q_origin(state)
                 
     def train(self, exp:Experiance):
         #Update the agent.
@@ -54,6 +73,7 @@ class DuelDDQNAgent():
         next_action = torch.argmax(next_q_values_main).item()
         #next_q_values_main.argmax(dim=1).item()
         
+        #q_values = torch.max(self.pred_model(exp.state, "main"))
         q_values = torch.max(self.pred_model(exp.state, "main"))
         next_q_value = next_q_values_target[next_action]
         target_q_value = exp.reward + (1 - exp.terminated) * self.discount_factor * next_q_value
